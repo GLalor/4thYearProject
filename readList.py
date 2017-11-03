e8cb9bd83541e1b246c2c57c093396329a3369cd
Graham Lalor <grahamlalor@gmail.com>
2017-11-03 18:56:01 +0000

    added correct version of readList.py

---
+++ readList.py
@@ -0,0 +1,15 @@
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import json, urllib
+url = 'https://dimon.ca/api/snp500' # Set destination URL here
+post_fields = {'session': '600-6-efbebfbcc'}     # Set POST fields here
+
+request = Request(url, urlencode(post_fields).encode())
+data = urlopen(request).read().decode()
+data = json.loads(data)
+for item in data['members']:
+    print(item['sym'])
+	
+with urllib.request.urlopen("https://www.barchart.com/stocks/quotes/MMM/volatility-greeks") as url:
+    data = url.read().decode("utf8")
+    print(data)
