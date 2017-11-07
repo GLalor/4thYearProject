e9ac6a373798d5a2fb12900b7e75bc61c2fcd67a
Graham Lalor <grahamlalor@gmail.com>
2017-11-07 17:33:50 +0000

    Added put calculations and removed nested for loops

--- optionChainRetrieval.py
+++ optionChainRetrieval.py
@@ -1,73 +1,93 @@
-import json
-import datetime
-import random 
-import math 
-import time
-import datetime
-from operator import add
-import matplotlib.pyplot as plt                         #mathplotlib
-import sys
+import json, datetime, random, math, time
 from urllib.request import urlopen
+#from operator import add
+#import matplotlib.pyplot as plt  # mathplotlib
 
 def main(ticker):
-	STRIKE_PRICE = 0        # S(T) price at maturity
-	CURRENT_VALUE =0		# S(0) spot price, price of stock now
-	VOLATILITY = .3672 			# sigma i.e. volatility of underlying stock
-	RISK_FREE_RATE = 2.1024 # mu
-	NO_SIMULATIONS = 10000
-	START_DATE = datetime.date.today()
-	EXPIRES = 55	# Number of days until maturity date
-	if "." in ticker: # some tickers in list have "." when not needed
-		ticker = ticker.replace(".", "") # Removing "."
-	url = "https://query2.finance.yahoo.com/v7/finance/options/"
-	url += ticker
+    option_type = "not set"
+    strike_price = 0        # S(T) price at maturity
+    current_value = 0		# S(0) spot price, price of stock now
+    volatility = .3672 			# sigma i.e. volatility of underlying stock
+    risk_free_rate = 2.1024  # mu
+    expires = 55  # Number of days until maturity date
+    if "." in ticker:  # some tickers in list have "." when not needed
+        ticker = ticker.replace(".", "")  # Removing "."
+    url = "https://query2.finance.yahoo.com/v7/finance/options/"
+    url += ticker
 
-	print(url)	# Prints URL to option chain
+    print(url)  # Prints URL to option chain
 
-	data = urlopen(url)
-	data = json.loads(data.read().decode())
-	for x in data['optionChain']['result']:
-		CURRENT_VALUE = x['quote']['regularMarketPrice']
-		for y in x['options']:
-			if not y['calls']:
-				for option in y['calls']:
-					STRIKE_PRICE = option['strike']	        # S(T) price at maturity
-					#VOLATILITY = option['impliedVolatility']	
-				
-	for x in range(0,5):
-		# W(T) Wiener process/Brownian motion  = math.sqrt(T) * random.gauss(0, 1.0)
-		# sequential approach, calculate option price every day until expiry
-		option_prices = []
-		times = []
-		for i in range(1, EXPIRES + 1):                                                 # 1 .. EXPIRES inclusive
-				sim_results = []                                                        # results from each simulation step
-				T = i/365               # days in the future
-				times.append(i)
-				for j in range(NO_SIMULATIONS):
-					sim_results.append(sim_option_price(time.time() + j,CURRENT_VALUE,
-														RISK_FREE_RATE, VOLATILITY,T,STRIKE_PRICE))
+    data = urlopen(url)
+    data = json.loads(data.read().decode())
+    for item in data['optionChain']['result']:
+        current_value = item['quote']['regularMarketPrice']
+        data = item['options']
+    for option in data:
+        calls, puts = option['calls'], option['puts']
+    for call in calls:
+        option_type = "Call"
+        strike_price = call['strike']	        # S(T) price at maturity
+        volatility = call['impliedVolatility']
+        runSimulaion(option_type, strike_price, current_value,
+                     volatility, risk_free_rate, expires, ticker)
 
-				discount_factor = math.exp(-RISK_FREE_RATE * T)                         # e to the power of ()
-				option_prices.append(discount_factor * (sum(sim_results) / float(NO_SIMULATIONS)))
-				print (ticker," ","Option Price ", option_prices[i-1], " at ", START_DATE + datetime.timedelta(days=i))
-		# Code to plot results to a graph
-		# plt.plot(times, option_prices)
-		# plt.xlabel('T')
-		# plt.ylabel('Option Prices')
-		# plt.show()	
+    for put in puts:
+        option_type = "Put"
+        strike_price = put['strike']	        # S(T) price at maturity
+        volatility = put['impliedVolatility']
+        runSimulaion(option_type, strike_price, current_value,
+                     volatility, risk_free_rate, expires, ticker)
+
+
+def runSimulaion(option_type, strike_price, current_value, volatility, risk_free_rate, expires, ticker):
+    start_date = datetime.date.today()
+    num_simulations = 10000
+    for x in range(0, 5):
+        # W(T) Wiener process/Brownian motion  = math.sqrt(T) * random.gauss(0, 1.0)
+        # sequential approach, calculate option price every day until expiry
+        option_prices = []
+        times = []
+        # 1 .. expires inclusive
+        for i in range(1, expires + 1):
+            # results from each simulation step
+            sim_results = []
+            T = i / 365               # days in the future
+            times.append(i)
+            for j in range(num_simulations):
+                sim_results.append(sim_option_price(time.time() + j, current_value,
+                									risk_free_rate, volatility, T, strike_price, option_type))
+
+            # e to the power of ()
+            discount_factor = math.exp(-risk_free_rate * T)
+            option_prices.append(
+                discount_factor * (sum(sim_results) / float(num_simulations)))
+            print(ticker, " ", option_type, " ", "Option Price ",
+                  option_prices[i - 1], " at ", start_date + datetime.timedelta(days=i))
+        # Code to plot results to a graph
+        # plt.plot(times, option_prices)
+        # plt.xlabel('T')
+        # plt.ylabel('Option Prices')
+        # plt.show()
 
 # european or asian call price
-def call_payoff(asset_price, STRIKE_PRICE): 
-	return max(0.0, asset_price - STRIKE_PRICE)
+def call_payoff(asset_price, strike_price):
+    return max(0.0, asset_price - strike_price)
+
+
+def put_payoff(asset_price, strike_price):
+    return max(0.0, strike_price - asset_price)
 
 # simulate the option price
-def sim_option_price(seed,CURRENT_VALUE,RISK_FREE_RATE,VOLATILITY,T,STRIKE_PRICE): 
-	random.seed(seed) 
-	asset_price = CURRENT_VALUE * math.exp((RISK_FREE_RATE - .5 * VOLATILITY**2) * T + VOLATILITY * math.sqrt(T) * random.gauss(0,1.0)) 
-	return call_payoff(asset_price, STRIKE_PRICE)
+def sim_option_price(seed, current_value, risk_free_rate, volatility, T, strike_price, option_type):
+    random.seed(seed)
+    asset_price = current_value * \
+    math.exp((risk_free_rate - .5 * volatility**2) * T +
+            volatility * math.sqrt(T) * random.gauss(0, 1.0))
+    if option_type == "Call":
+        return call_payoff(asset_price, strike_price)
+    else:
+        return put_payoff(asset_price, strike_price)
 
-# Stops code being run on import	
+# Stops code being run on import
 if __name__ == "__main__":
-	main()
-			
-			
+    main()
