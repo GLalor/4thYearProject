e685d8ce53ed024260340441cc51e2540ce4faff
Graham Lalor <grahamlalor@gmail.com>
2018-02-08 18:11:35 +0000

    change write to hadoop to a specific foler, createURL bug, added gitignore (not sure if will work)

--- optionChainRetrieval.py
+++ optionChainRetrieval.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 import findspark
 
 findspark.init("C:\spark-2.2.1-bin-hadoop2.7")
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import *
 sparkSession = SparkSession.builder.appName("option-pricer-write-to-hadoop").getOrCreate()
 #Dictionaries for json output
@@ -143,9 +143,10 @@ def createYahooUrl(optionTicker):
 		expirationDates = data['optionChain']['result'][0]['expirationDates']
 		for item in expirationDates:
 			dt = datetime.datetime.fromtimestamp(item) - datetime.datetime.now()
-			if dt.days > 0:
+			if dt.days > 0: # should run he day before bu is seen as 0 days and number of hours
 				expDate = item
-			break
+				break
+		return url + "?date=" + str(expDate)
 	except urllib.error.HTTPError as err:
 		if err.code == 404:
 			print("Page not found!")
@@ -155,17 +156,17 @@ def createYahooUrl(optionTicker):
 			print("Something happened! Error code", err.code)
 	except urllib.error.URLError as err:
 		print("Some other error happened:", err.reason)
-	return url + "?date=" + str(expDate)
+	
 
 def writeResultHive():
     option_prices_data = sparkSession.read.json('optionPrices.json')
-    option_prices_data.createOrReplaceTempView("option_prices_data")
-    option_prices_data.cache()
-    sparkSession.sql("DROP TABLE IF EXISTS option_prices_data_table")
-    sparkSession.table("option_prices_data").write.saveAsTable("option_prices_data_table")
+    option_prices_data.write.save('E:\ProjectDB', format='json', mode='append')
+
+    # sparkSession.sql("DROP TABLE IF EXISTS option_prices_data_table")
+    # sparkSession.table("option_prices_data").write.saveAsTable("option_prices_data_table")
     #  USE TO TEST DB
-    # resultsHiveDF = sparkSession.sql("SELECT * FROM option_prices_data_table")
-    # resultsHiveDF.show(1)
+    resultsHiveDF = sparkSession.read.format('json').load('E:\ProjectDB') 
+    resultsHiveDF.show(1)
 
 # Stops code being run on import
 if __name__ == "__main__":
